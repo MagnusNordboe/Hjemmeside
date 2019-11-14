bdc0fffc777e00abc1dcfb7c4c2fced05f5322ba
Magnus <nordbom@live.no>
2019-11-14 19:42:36 +0100

    Fikset på plassering av bilde

--- dist/main.70f2f24c.js
+++ dist/main.70f2f24c.js
@@ -123,7 +123,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Hello Bulma!');
 });
-},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -151,7 +151,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51941" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50574" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -182,9 +182,8 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
         assetsToAccept.forEach(function (v) {
           hmrAcceptRun(v[0], v[1]);
         });
-      } else if (location.reload) {
-        // `location` global exists in a web worker context but lacks `.reload()` function.
-        location.reload();
+      } else {
+        window.location.reload();
       }
     }
 
@@ -327,5 +326,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","lib/main.js"], null)
+},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","lib/main.js"], null)
 //# sourceMappingURL=/main.70f2f24c.js.map
